ed1102afd0c57b8e0296d5a09d47c6dd0f55fb48
hantsy <hantsy@gmail.com>
2017-08-19 16:52:43 +0800

    added samples for rxjava and rxjava2

---
+++ rxjava2/src/main/java/com/example/demo/RxJava2PostRepository.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example.demo;
+
+import io.reactivex.Observable;
+import io.reactivex.Single;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import org.springframework.stereotype.Component;
+
+
+/**
+ *
+ * @author hantsy
+ */
+@Component
+class RxJava2PostRepository {
+
+    private static final Map<Long, Post> DATA = new HashMap<>();
+    private static long ID_COUNTER = 1L;
+
+    static {
+        Arrays.asList("First Post", "Second Post")
+            .stream()
+            .forEach((java.lang.String title) -> {
+                long id = ID_COUNTER++;
+                DATA.put(Long.valueOf(id), Post.builder().id(id).title(title).content("content of " + title).build());
+            }
+            );
+    }
+
+    Observable<Post> findAll() {
+        return Observable.fromIterable(DATA.values());
+    }
+
+    Single<Post> findById(Long id) {
+        return Single.just(DATA.get(id));
+    }
+
+    Single<Post> createPost(Post post) {
+        long id = ID_COUNTER++;
+        post.setId(id);
+        DATA.put(id, post);
+        return Single.just(post);
+    }
+
+}
