1d785b07310fff67a9074ea1b8bcecc02b565b1a
hantsy <hantsy@gmail.com>
2017-08-24 15:08:54 +0800

    setId manually in data-cassandra/src/main/java/com/example/demo/PostController.java

--- data-cassandra/src/main/java/com/example/demo/PostController.java
+++ data-cassandra/src/main/java/com/example/demo/PostController.java
@@ -5,6 +5,7 @@
  */
 package com.example.demo;
 
+import java.util.UUID;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -37,6 +38,7 @@ class PostController {
 
     @PostMapping("")
     public Mono<Post> create(@RequestBody Post post) {
+        post.setId(UUID.randomUUID().toString());
         return this.posts.save(post);
     }
 
