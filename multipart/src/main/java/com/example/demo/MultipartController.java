777f1a5ba13204e28033c6597572d5c4f94dff94
hantsy <hantsy@gmail.com>
2017-08-25 11:39:48 +0800

    added multipart sample

---
+++ multipart/src/main/java/com/example/demo/MultipartController.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example.demo;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import org.springframework.http.MediaType;
+import org.springframework.http.codec.multipart.FilePart;
+import org.springframework.http.codec.multipart.Part;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+/**
+ *
+ * @author hantsy
+ */
+@RestController
+@RequestMapping("/uploads")
+public class MultipartController {
+
+    //			@RequestPart("fileParts") FilePart fileParts,
+//				@RequestPart("fileParts") Mono<FilePart> filePartsMono,
+//				@RequestPart("fileParts") Flux<FilePart> filePartsFlux,
+//    @PostMapping("/requestBodyMap")
+//    Mono<String> requestBodyMap(@RequestBody Mono<MultiValueMap<String, Part>> partsMono) {
+//        return partsMono.map(this::partMapDescription);
+//    }
+
+    @PostMapping(value = "", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    Mono<String> requestBodyFlux(@RequestBody Flux<Part> parts) {
+        return partFluxDescription(parts);
+    }
+
+    private static String partMapDescription(MultiValueMap<String, Part> partsMap) {
+        return partsMap.keySet().stream().sorted()
+            .map(key -> partListDescription(partsMap.get(key)))
+            .collect(Collectors.joining(",", "Map[", "]"));
+    }
+
+    private static Mono<String> partFluxDescription(Flux<? extends Part> partsFlux) {
+        return partsFlux.log().collectList().map(MultipartController::partListDescription);
+    }
+
+    private static String partListDescription(List<? extends Part> parts) {
+        return parts.stream().map(MultipartController::partDescription)
+            .collect(Collectors.joining(",", "[", "]"));
+    }
+
+    private static String partDescription(Part part) {
+        return part instanceof FilePart ? part.name() + ":" + ((FilePart) part).filename() : part.name();
+    }
+
+}
