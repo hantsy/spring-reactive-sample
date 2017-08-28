62e2486b88477bc0f9ae01a0185eb4119a1de806
hantsy <hantsy@gmail.com>
2017-08-28 17:54:27 +0800

    added mvc for reactive Thymeleaf template resolver.

---
+++ mvc/src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example.demo;
+
+import java.time.Duration;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.thymeleaf.spring5.context.webflux.ReactiveDataDriverContextVariable;
+import reactor.core.publisher.Flux;
+
+/**
+ *
+ * @author hantsy
+ */
+@Controller
+public class HomeController {
+
+    private final PostRepository posts;
+
+    HomeController(PostRepository posts) {
+        this.posts = posts;
+    }
+
+    @GetMapping("/home")
+    public String home(final Model model) {
+
+        Flux<Post> postList = this.posts.findAll();
+        model.addAttribute("posts", new ReactiveDataDriverContextVariable(postList, 100));
+        return "home";
+    }
+
+    @GetMapping("/freemarker")
+    public String freemarker(final Model model) {
+
+        Flux<Post> postList = this.posts.findAll();
+        model.addAttribute("posts", postList.collectList().block(Duration.ofSeconds(10)));
+        return "freemarker";
+    }
+}
