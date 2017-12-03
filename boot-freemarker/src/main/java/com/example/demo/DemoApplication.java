633eb1ff12196725cad1984e73f3c50589c464f3
hantsy <hantsy@gmail.com>
2017-12-03 15:54:00 +0800

    clean boot-freemarker sample

--- boot-freemarker/src/main/java/com/example/demo/DemoApplication.java
+++ boot-freemarker/src/main/java/com/example/demo/DemoApplication.java
@@ -8,43 +8,19 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.authorization.AuthorizationDecision;
-import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
-import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.web.server.SecurityWebFilterChain;
-import org.springframework.security.web.server.authorization.AuthorizationContext;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.reactive.result.view.freemarker.FreeMarkerConfigurer;
-import org.springframework.web.reactive.result.view.freemarker.FreeMarkerViewResolver;
 import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
 
 @SpringBootApplication
 @EnableMongoAuditing
@@ -53,62 +29,6 @@ public class DemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
-
-}
-
-@Configuration
-class WebConfig {
-
-    @Autowired
-    ApplicationContext ctx;
-
-    @Bean
-    public FreeMarkerConfigurer freeMarkerConfig() {
-        FreeMarkerConfigurer configurer = new FreeMarkerConfigurer();
-        configurer.setPreferFileSystemAccess(false);
-        configurer.setTemplateLoaderPath("classpath:/templates/");
-        configurer.setResourceLoader(this.ctx);
-        return configurer;
-    }
-
-    @Bean
-    public FreeMarkerViewResolver freeMarkerViewResolver() {
-        final FreeMarkerViewResolver freeMarkerViewResolver = new FreeMarkerViewResolver("", ".ftl");
-        freeMarkerViewResolver.setOrder(1);
-        return freeMarkerViewResolver;
-    }
-
-}
-
-@EnableWebFluxSecurity
-class SecurityConfig {
-
-    @Bean
-    SecurityWebFilterChain springWebFilterChain(ServerHttpSecurity http) throws Exception {
-        return http
-                .authorizeExchange()
-                .pathMatchers(HttpMethod.GET, "/posts/**").permitAll()
-                .pathMatchers(HttpMethod.DELETE, "/posts/**").hasRole("ADMIN")
-                .pathMatchers("/posts/**").authenticated()
-                //.pathMatchers("/users/{user}/**").access(this::currentUserMatchesPath)
-                .anyExchange().permitAll()
-                .and()
-                .build();
-    }
-
-    private Mono<AuthorizationDecision> currentUserMatchesPath(Mono<Authentication> authentication, AuthorizationContext context) {
-        return authentication
-                .map(a -> context.getVariables().get("user").equals(a.getName()))
-                .map(granted -> new AuthorizationDecision(granted));
-    }
-
-    @Bean
-    public MapReactiveUserDetailsService userDetailsService() {
-        UserDetails user = User.withDefaultPasswordEncoder().username("test").password("password").roles("USER").build();
-        UserDetails admin = User.withDefaultPasswordEncoder().username("admin").password("password").roles("USER", "ADMIN").build();
-        return new MapReactiveUserDetailsService(user, admin);
-    }
-
 }
 
 @Component
@@ -125,19 +45,20 @@ class DataInitializer implements CommandLineRunner {
     public void run(String[] args) {
         log.info("start data initialization  ...");
         this.posts
-                .deleteAll()
-                .thenMany(
-                        Flux.just("Post one", "Post two")
-                                .flatMap(
-                                        title -> this.posts.save(Post.builder().title(title).content("content of " + title).build())
-                                )
-                )
-                .log()
-                .subscribe(
-                        null,
-                        null,
-                        () -> log.info("done initialization...")
-                );
+            .deleteAll()
+            .thenMany(
+                Flux
+                    .range(1, 1000)
+                    .flatMap(
+                        num -> this.posts.save(Post.builder().title("Title" + num).content("content of " + "Title" + num).build())
+                    )
+            )
+            .log()
+            .subscribe(
+                null,
+                null,
+                () -> log.info("done initialization...")
+            );
 
     }
 
@@ -163,52 +84,6 @@ class HomeController {
         model.addAttribute("hello", "Hi, Freemarker");
         return "hello";
     }
-}
-
-@RestController()
-@RequestMapping(value = "/posts")
-class PostController {
-
-    private final PostRepository posts;
-
-    public PostController(PostRepository posts) {
-        this.posts = posts;
-    }
-
-    @GetMapping("")
-    public Flux<Post> all() {
-        return this.posts.findAll();
-    }
-
-    @PostMapping("")
-    public Mono<Post> create(@RequestBody Post post) {
-        return this.posts.save(post);
-    }
-
-    @GetMapping("/{id}")
-    public Mono<Post> get(@PathVariable("id") String id) {
-        return this.posts.findById(id);
-    }
-
-    @PutMapping("/{id}")
-    public Mono<Post> update(@PathVariable("id") String id, @RequestBody Post post) {
-        return this.posts.findById(id)
-                .map(
-                        p -> {
-                            p.setTitle(post.getTitle());
-                            p.setContent(post.getContent());
-
-                            return p;
-                        }
-                )
-                .flatMap(p -> this.posts.save(p));
-    }
-
-    @DeleteMapping("/{id}")
-    public Mono<Void> delete(@PathVariable("id") String id) {
-        return this.posts.deleteById(id);
-    }
-
 }
 
 interface PostRepository extends ReactiveMongoRepository<Post, String> {
