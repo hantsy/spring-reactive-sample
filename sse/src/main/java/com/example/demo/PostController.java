c8494c4c66026d2ac893db4fcada7c0fc336c123
hantsy <hantsy@gmail.com>
2017-12-18 22:37:37 +0800

    clean the sse sample.

--- sse/src/main/java/com/example/demo/PostController.java
+++ sse/src/main/java/com/example/demo/PostController.java
@@ -8,18 +8,14 @@ package com.example.demo;
 import java.time.Duration;
 import java.util.Date;
 import java.util.stream.Stream;
+
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.util.function.Tuple2;
 
 /**
- *
  * @author hantsy
  */
 @RestController
@@ -32,33 +28,25 @@ class PostController {
         this.posts = posts;
     }
 
-    @GetMapping(value = "")
+    @GetMapping(produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public Flux<Post> all() {
         return this.posts.findAll();
     }
 
-    @GetMapping(value = "/stream", produces = MediaType.APPLICATION_STREAM_JSON_VALUE)
-    public Flux<Post> allStream() {
-        return Flux.interval(Duration.ofSeconds(1L)).flatMap((oneSecond)->this.posts.findAll());
+    @GetMapping(produces = MediaType.APPLICATION_STREAM_JSON_VALUE)
+    public Flux<Post> stream() {
+        return Flux.interval(Duration.ofSeconds(1L)).flatMap((oneSecond) -> this.posts.findAll());
     }
 
-    @GetMapping(value = "/{id}")
-    public Mono<Post> get(@PathVariable(value = "id") Long id) {
-        return this.posts.findById(id);
+    @GetMapping(produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<Post> sse() {
+        return Flux
+                .zip(Flux.interval(Duration.ofSeconds(1)), this.posts.findAll().repeat())
+                .map(Tuple2::getT2);
     }
 
-    @GetMapping(value = "/{id}/events", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-    public Flux<PostEvent> getEvents(@PathVariable(value = "id") Long id) {
-
-        return this.posts.findById(id).flatMapMany(post -> {
-            Flux<Long> interval = Flux.interval(Duration.ofSeconds(1));
-            Flux<PostEvent> postEventFlux = Flux.fromStream(Stream.generate(() -> new PostEvent(post, new Date())));
-            return Flux.zip(interval, postEventFlux).map(Tuple2::getT2);
-        });
-    }
-
-    @PostMapping(value = "")
-    public Mono<Post> create(Post post) {
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public Mono<Post> create(@RequestBody Post post) {
         return this.posts.save(post);
     }
 
