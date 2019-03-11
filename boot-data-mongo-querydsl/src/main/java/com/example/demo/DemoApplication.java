21547188ac54881a4d24f22d30d1705a2a2f5d35
hantsy <hantsy@gmail.com>
2019-03-11 21:35:26 +0800

    added reactive data-mongo querydsl sample.

---
+++ boot-data-mongo-querydsl/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,132 @@
+package com.example.demo;
+
+import lombok.*;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.data.querydsl.ReactiveQuerydslPredicateExecutor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+}
+
+@Component
+@Slf4j
+class DataInitializer implements CommandLineRunner {
+
+    private final PostRepository posts;
+
+    public DataInitializer(PostRepository posts) {
+        this.posts = posts;
+    }
+
+    @Override
+    public void run(String[] args) {
+        log.info("start data initialization  ...");
+        this.posts
+            .deleteAll()
+            .thenMany(
+                Flux
+                    .just("Post one", "Post two")
+                    .flatMap(
+                        title -> this.posts.save(Post.builder().title(title).content("content of " + title).build())
+                    )
+            )
+            .log()
+            .subscribe(
+                null,
+                null,
+                () -> log.info("done initialization...")
+            );
+
+    }
+
+}
+
+@RestController()
+@RequestMapping(value = "/posts")
+class PostController {
+
+    private final PostRepository posts;
+
+    public PostController(PostRepository posts) {
+        this.posts = posts;
+    }
+
+    @GetMapping("")
+    public Flux<Post> all(@RequestParam("q") String q) {
+        return Optional.ofNullable(q)
+            .map(
+                keyword -> this.posts.findAll(QPost.post.title.containsIgnoreCase(keyword)
+                    .or(QPost.post.content.containsIgnoreCase(keyword)))
+            ).orElse(
+                this.posts.findAll()
+            );
+    }
+
+    @PostMapping("")
+    public Mono<Post> create(@RequestBody Post post) {
+        return this.posts.save(post);
+    }
+
+    @GetMapping("/{id}")
+    public Mono<Post> get(@PathVariable("id") String id) {
+        return this.posts.findOne(QPost.post.id.eq(id));
+    }
+
+    @PutMapping("/{id}")
+    public Mono<Post> update(@PathVariable("id") String id, @RequestBody Post post) {
+        return this.posts.findById(id)
+            .map(p -> {
+                p.setTitle(post.getTitle());
+                p.setContent(post.getContent());
+
+                return p;
+            })
+            .flatMap(this.posts::save);
+    }
+
+    @DeleteMapping("/{id}")
+    public Mono<Void> delete(@PathVariable("id") String id) {
+        return this.posts.deleteById(id);
+    }
+
+}
+
+
+interface PostRepository extends ReactiveMongoRepository<Post, String>, ReactiveQuerydslPredicateExecutor<Post> {
+}
+
+@Document
+@Data
+@ToString
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+class Post {
+
+    @Id
+    private String id;
+    private String title;
+    private String content;
+
+    @CreatedDate
+    private LocalDateTime createdDate;
+}
