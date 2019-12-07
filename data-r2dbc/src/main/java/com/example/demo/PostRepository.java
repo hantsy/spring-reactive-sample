a787289e26d4708a0feb794ab593f7b2dbc2b0e4
hantsy <hantsy@gmail.com>
2019-12-07 15:40:18 +0800

    clean up data-r2dbc example

--- data-r2dbc/src/main/java/com/example/demo/PostRepository.java
+++ data-r2dbc/src/main/java/com/example/demo/PostRepository.java
@@ -6,7 +6,7 @@
 package com.example.demo;
 
 
-import org.springframework.data.r2dbc.repository.query.Query;
+import org.springframework.data.r2dbc.repository.Query;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import reactor.core.publisher.Flux;
 
