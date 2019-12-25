c218b5532ebde98104e3d35264567f07875e9ca6
hantsy <hantsy@gmail.com>
2019-12-25 23:40:34 +0800

    add integration test

--- boot-data-neo4j-rx/src/test/java/com/example/demo/DemoApplicationTests.java
+++ boot-data-neo4j-rx/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,24 @@
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment= SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureWebTestClient
 class DemoApplicationTests {
+
+	@Autowired
+	WebTestClient client;
 
 	@Test
 	void contextLoads() {
+		this.client.get().uri("/posts")
+				.exchange()
+				.expectStatus().is2xxSuccessful()
+				.expectBodyList(Post.class).hasSize(2);
 	}
 
 }
