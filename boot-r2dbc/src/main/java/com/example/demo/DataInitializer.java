cfd1ea6de950140f66b93a8c106354a711519b70
hantsy <hantsy@gmail.com>
2020-02-15 15:18:31 +0800

    add transactionalOperator sample

--- boot-r2dbc/src/main/java/com/example/demo/DataInitializer.java
+++ boot-r2dbc/src/main/java/com/example/demo/DataInitializer.java
@@ -1,29 +1,30 @@
 package com.example.demo;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.r2dbc.core.DatabaseClient;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.reactive.TransactionalOperator;
 
 import static org.springframework.data.domain.Sort.Order.desc;
 
 
 @Component
 @Slf4j
+@RequiredArgsConstructor
 class DataInitializer implements ApplicationRunner {
 
     private final DatabaseClient databaseClient;
 
-    public DataInitializer(DatabaseClient databaseClient) {
-        this.databaseClient = databaseClient;
-    }
+    private final TransactionalOperator transactionalOperator;
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
         log.info("start data initialization...");
-        this.databaseClient.insert()
+        transactionalOperator.execute(status -> this.databaseClient.insert()
                 .into("posts")
                 //.nullValue("id", Integer.class)
                 .value("title", "First post title")
@@ -39,6 +40,6 @@ class DataInitializer implements ApplicationRunner {
                                 .all()
                                 .log()
                 )
-                .subscribe(null, null, () -> log.info("initialization is done..."));
+        ).subscribe(null, null, () -> log.info("initialization is done..."));
     }
 }
--- boot-r2dbc/src/main/resources/data.sql
+++ boot-r2dbc/src/main/resources/data.sql
@@ -1,2 +1,2 @@
 DELETE FROM posts;
-INSERT INTO  posts (title, content) VALUES ('post one', 'content of post one');
+--INSERT INTO  posts (title, content) VALUES ('post one', 'content of post one');
