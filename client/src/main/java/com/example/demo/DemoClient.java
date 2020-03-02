2e9ea176f88a93e3b9fe6b33398508066c004298
hantsy <hantsy@gmail.com>
2020-03-02 14:24:54 +0800

    update client.

--- client/src/main/java/com/example/demo/DemoClient.java
+++ client/src/main/java/com/example/demo/DemoClient.java
@@ -6,7 +6,12 @@
 package com.example.demo;
 
 import java.io.IOException;
-import org.springframework.web.reactive.function.client.WebClient;
+
+import org.springframework.http.client.Netty4ClientHttpRequestFactory;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import org.springframework.http.codec.json.Jackson2JsonDecoder;
+import org.springframework.http.codec.json.Jackson2JsonEncoder;
+import org.springframework.web.reactive.function.client.*;
 
 /**
  *
@@ -15,7 +20,32 @@ import org.springframework.web.reactive.function.client.WebClient;
 public class DemoClient {
 
     public static final void main(String[] args) throws IOException {
-        WebClient client = WebClient.create("http://localhost:8080");
+        WebClient client = WebClient.builder()
+                //see: https://github.com/jetty-project/jetty-reactive-httpclient
+                //.clientConnector(new JettyClientHttpConnector())
+                .clientConnector(new ReactorClientHttpConnector())
+                .codecs(
+                        clientCodecConfigurer ->{
+                            // use defaultCodecs() to apply DefaultCodecs
+                            // clientCodecConfigurer.defaultCodecs();
+
+                            // alter a registered encoder/decoder based on the default config.
+                            // clientCodecConfigurer.defaultCodecs().jackson2Encoder(...)
+
+                            // Or
+                            // use customCodecs to register Codecs from scratch.
+                            clientCodecConfigurer.customCodecs().register(new Jackson2JsonDecoder());
+                            clientCodecConfigurer.customCodecs().register(new Jackson2JsonEncoder());
+                        }
+
+                )
+                .exchangeStrategies(ExchangeStrategies.withDefaults())
+//                .exchangeFunction(ExchangeFunctions.create(new ReactorClientHttpConnector())
+//                        .filter(ExchangeFilterFunction.ofRequestProcessor(clientRequest -> {})))
+//                .filter(ExchangeFilterFunction.ofRequestProcessor(clientRequest -> {clientRequest.}))
+                //.defaultHeaders(httpHeaders -> httpHeaders.addAll())
+                .baseUrl("http://localhost:8080")
+                .build();
         client
             .get()
             .uri("/posts")
