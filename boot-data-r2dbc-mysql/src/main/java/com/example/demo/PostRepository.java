7a31542cb471bfd0630c9f0288eaea84ad16d344
hantsy <hantsy@gmail.com>
2020-08-23 14:14:25 +0800

    added TestcontrainersPostRepositoryTest.java in boot-data-r2dbc-mysql

--- boot-data-r2dbc-mysql/src/main/java/com/example/demo/PostRepository.java
+++ boot-data-r2dbc-mysql/src/main/java/com/example/demo/PostRepository.java
@@ -7,12 +7,12 @@ package com.example.demo;
 
 
 import org.springframework.data.r2dbc.repository.Query;
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import org.springframework.data.r2dbc.repository.R2dbcRepository;
 import reactor.core.publisher.Flux;
 
-interface PostRepository extends ReactiveCrudRepository<Post, Integer> {
+interface PostRepository extends R2dbcRepository<Post, Integer> {
 
-    @Query("SELECT * FROM posts WHERE title like $1")
+    @Query("SELECT * FROM posts WHERE title like :name")
     Flux<Post> findByTitleContains(String name);
 
 }
