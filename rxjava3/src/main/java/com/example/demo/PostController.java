835a38a8a8d6ca3737306490f77bd8ba502775f6
hantsy <hantsy@gmail.com>
2020-10-07 19:40:38 +0800

    added rxjava3 example

---
+++ rxjava3/src/main/java/com/example/demo/PostController.java
@@ -0,0 +1,36 @@
+package com.example.demo;
+
+import io.reactivex.rxjava3.core.Maybe;
+import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.core.Single;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * @author hantsy
+ */
+@RestController
+@RequestMapping(value = "/posts")
+class PostController {
+
+    private final PostRepository posts;
+
+    public PostController(PostRepository posts) {
+        this.posts = posts;
+    }
+
+    @GetMapping(value = "")
+    public Observable<Post> all() {
+        return this.posts.findAll();
+    }
+
+    @GetMapping(value = "/{id}")
+    public Maybe<Post> get(@PathVariable(value = "id") Long id) {
+        return this.posts.findById(id);
+    }
+
+    @PostMapping(value = "")
+    public Single<Post> create(Post post) {
+        return this.posts.save(post);
+    }
+
+}
---
+++ rxjava3/src/main/java/com/example/demo/PostRepository.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example.demo;
+
+import io.reactivex.rxjava3.core.Flowable;
+import io.reactivex.rxjava3.core.Maybe;
+import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.core.Single;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.UUID;
+
+
+/**
+ * @author hantsy
+ */
+@Component
+class PostRepository {
+
+    private static final List<Post> DATA = new ArrayList<>();
+
+    static {
+        DATA.add(Post.builder().id(UUID.randomUUID()).title("post one").content("content of post one").build());
+        DATA.add(Post.builder().id(UUID.randomUUID()).title("post two").content("content of post two").build());
+    }
+
+    Observable<Post> findAll() {
+        return Observable.fromIterable(DATA);
+    }
+
+    Flowable<Post> flowable() {
+        return Flowable.fromIterable(DATA);
+    }
+
+    Maybe<Post> findById(Long id) {
+        return findAll().filter(p -> Objects.equals(p.getId(), id)).singleElement();
+    }
+
+    Single<Post> save(Post post) {
+        Post saved = Post.builder().id(UUID.randomUUID()).title(post.getTitle()).content(post.getContent()).build();
+        DATA.add(saved);
+        return Single.just(saved);
+    }
+
+}
