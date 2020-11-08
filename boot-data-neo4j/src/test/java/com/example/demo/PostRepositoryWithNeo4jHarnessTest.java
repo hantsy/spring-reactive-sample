3c59059180c0273f9c4cb2c9aa178dcc70a1a2ec
hantsy <hantsy@gmail.com>
2020-11-08 19:22:12 +0800

    test: add neo4j harness

---
+++ boot-data-neo4j/src/test/java/com/example/demo/PostRepositoryWithNeo4jHarnessTest.java
@@ -0,0 +1,108 @@
+package com.example.demo;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.*;
+import org.neo4j.harness.Neo4j;
+import org.neo4j.harness.Neo4jBuilders;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.neo4j.DataNeo4jTest;
+import org.springframework.data.domain.Example;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
+import java.io.IOException;
+import java.time.Duration;
+import java.util.Comparator;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+//@SpringBootTest
+//
+// @DataNeo4jTest does not work in Spring Data Neo4j 6.0 RC1 and Spring Boot 2.4.0-M3
+// see: https://github.com/spring-projects/spring-boot/issues/23630
+// a workaround is adding a `@Transactional(propagation = Propagation.NEVER)`
+@DataNeo4jTest
+@Transactional(propagation = Propagation.NEVER)
+@Slf4j
+public class PostRepositoryWithNeo4jHarnessTest {
+
+    private static Neo4j embeddedDatabaseServer;
+
+    @BeforeAll
+    static void initializeNeo4j() {
+
+        embeddedDatabaseServer = Neo4jBuilders.newInProcessBuilder()
+                .withDisabledServer()//disable http server
+                .build();
+    }
+
+    @DynamicPropertySource
+    static void neo4jProperties(DynamicPropertyRegistry registry) {
+
+        registry.add("spring.neo4j.uri", embeddedDatabaseServer::boltURI);
+        registry.add("spring.neo4j.authentication.username", () -> "neo4j");
+        registry.add("spring.neo4j.authentication.password", () -> null);
+    }
+
+    @AfterAll
+    static void stopNeo4j() {
+        embeddedDatabaseServer.close();
+    }
+
+    @Autowired
+    private PostRepository posts;
+
+    @BeforeEach
+    public void setup() throws IOException {
+        log.debug("running setup.....,");
+        this.posts.deleteAll()
+                .thenMany(testSaveMethod())
+                .log()
+                .thenMany(testFoundMethod())
+                .log()
+                .blockLast(Duration.ofSeconds(5));// to make the tests work
+    }
+
+    private Flux<Post> testSaveMethod() {
+        var data = Stream.of("Post one", "Post two")
+                .map(title -> Post.builder().title(title).content("The content of " + title).build())
+                .collect(Collectors.toList());
+        return Flux.fromIterable(data)
+                .flatMap(it -> this.posts.save(it));
+    }
+
+    private Flux<Post> testFoundMethod() {
+        return this.posts
+                .findAll(Example.of(Post.builder().title("Post one").build()));
+    }
+
+    @AfterEach
+    void teardown() {
+        //this.posts.deleteAll();
+    }
+
+    @Test
+    void testAllPosts() {
+        posts.findAll().sort(Comparator.comparing(Post::getTitle))
+                .as(StepVerifier::create)
+                .consumeNextWith(p -> assertEquals("Post one", p.getTitle()))
+                .consumeNextWith(p -> assertEquals("Post two", p.getTitle()))
+                .verifyComplete();
+    }
+
+    @Test
+    void testFindByQuery() {
+        posts.findByTitleContains("(?i).*" + "one" + ".*").sort(Comparator.comparing(Post::getTitle))
+                .as(StepVerifier::create)
+                .consumeNextWith(p -> assertEquals("Post one", p.getTitle()))
+                .verifyComplete();
+    }
+
+}
