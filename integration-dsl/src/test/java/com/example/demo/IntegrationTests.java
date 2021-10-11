1a1c438d6f22010f18adaa7c7361b573aa673f0a
Raja Dilip Kolli <rajadileepkolli@users.noreply.github.com>
2021-10-11 20:59:27 +0530

    fix issues with integration DSL project (#31)

--- integration-dsl/src/test/java/com/example/demo/IntegrationTests.java
+++ integration-dsl/src/test/java/com/example/demo/IntegrationTests.java
@@ -9,10 +9,13 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.netty.DisposableServer;
+import reactor.netty.http.server.HttpServer;
 
 import java.time.Duration;
 
@@ -29,7 +32,8 @@ public class IntegrationTests {
 
     WebTestClient rest;
 
-    @Autowired HttpServer httpServer;
+    @Autowired
+    HttpServer httpServer;
 
     private DisposableServer disposableServer;
 
