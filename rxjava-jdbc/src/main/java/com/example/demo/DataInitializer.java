fca2b90dedfc8eb5fe20885ee3385519457e62b6
hantsy <hantsy@gmail.com>
2021-11-12 10:36:22 +0800

    chore: clean the deps

--- rxjava-jdbc/src/main/java/com/example/demo/DataInitializer.java
+++ rxjava-jdbc/src/main/java/com/example/demo/DataInitializer.java
@@ -7,7 +7,6 @@ package com.example.demo;
 
 import java.util.UUID;
 import java.util.stream.Stream;
-import javax.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
