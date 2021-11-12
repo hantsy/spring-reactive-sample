d5355e035cfcd3b84e6ee5bb67dca0adaaa65407
hantsy <hantsy@gmail.com>
2021-11-12 16:33:03 +0800

    chore: clean groovy codes

--- groovy/src/test/groovy/com/example/demo/ApplicationTests.groovy
+++ groovy/src/test/groovy/com/example/demo/ApplicationTests.groovy
@@ -4,15 +4,18 @@ import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.ApplicationContext
+import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig
 import org.springframework.test.web.reactive.server.WebTestClient
 
 @SpringJUnitConfig(classes = [Application.class])
+@ActiveProfiles("test")
 class ApplicationTests {
 
     @Autowired
-    ApplicationContext context;
-    WebTestClient client;
+    ApplicationContext context
+
+    WebTestClient client
 
     @BeforeEach
     void setup() {
