37787ee81837507236ec231e893330e0af65672b
hantsy <hantsy@gmail.com>
2021-11-13 14:00:00 +0800

    chore: clean codes

--- integration-dsl/src/test/java/com/example/demo/PostControllerTest.java
+++ integration-dsl/src/test/java/com/example/demo/PostControllerTest.java
@@ -5,22 +5,20 @@
  */
 package com.example.demo;
 
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import static org.springframework.http.MediaType.APPLICATION_JSON;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 import org.springframework.test.web.reactive.server.WebTestClient;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON;
 
 /**
  *
  * @author hantsy
  */
-@RunWith(SpringRunner.class)
-@ContextConfiguration(classes = Application.class)
+@SpringJUnitConfig(classes = Application.class)
 @ActiveProfiles("test")
 public class PostControllerTest {
 
@@ -29,7 +27,7 @@ public class PostControllerTest {
 
     WebTestClient rest;
 
-    @Before
+    @BeforeEach
     public void setup() {
         this.rest = WebTestClient
             .bindToController(this.ctrl)
