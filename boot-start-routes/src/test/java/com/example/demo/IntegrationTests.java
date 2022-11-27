44394adcd8e04defc5edb003a694fac4ad4defcd
hantsy <hantsy@gmail.com>
2022-11-27 13:43:06 +0800

    chore: clean codes

--- boot-start-routes/src/test/java/com/example/demo/IntegrationTests.java
+++ boot-start-routes/src/test/java/com/example/demo/IntegrationTests.java
@@ -3,7 +3,7 @@ package com.example.demo;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
