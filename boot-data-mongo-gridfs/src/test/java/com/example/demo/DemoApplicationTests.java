162a7ad94d4958211fa91149e5f69327cb547a41
hantsy <hantsy@gmail.com>
2022-11-27 15:55:46 +0800

    chore: clean codes

--- boot-data-mongo-gridfs/src/test/java/com/example/demo/DemoApplicationTests.java
+++ boot-data-mongo-gridfs/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -5,7 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.client.MultipartBodyBuilder;
