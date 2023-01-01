bcd284961ae5f3676effbd17bc40a0327aeaf1c2
hantsy <hantsy@gmail.com>
2023-01-01 17:38:09 +0800

    chore: clean integration-dsl codes

--- integration-dsl/src/main/java/com/example/demo/IntegrationDslConfig.java
+++ integration-dsl/src/main/java/com/example/demo/IntegrationDslConfig.java
@@ -8,54 +8,54 @@ package com.example.demo;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.integration.annotation.IntegrationComponentScan;
 import org.springframework.integration.config.EnableIntegration;
 import org.springframework.integration.dsl.IntegrationFlow;
-import org.springframework.integration.dsl.IntegrationFlows;
 import org.springframework.integration.webflux.dsl.WebFlux;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.util.UriComponentsBuilder;
 
 /**
- *
  * @author hantsy
  */
 @Configuration
 @EnableIntegration
+@IntegrationComponentScan
 public class IntegrationDslConfig {
 
     @Bean
     public IntegrationFlow inboundChannelAdapterFlow() {
-        return IntegrationFlows
-            .from(
-                WebFlux
-                    .inboundGateway("/all")
-                    .requestMapping(m -> m.methods(HttpMethod.GET))
-                    // .requestPayloadType(ResolvableType.forClassWithGenerics(Flux.class, String.class))
-                    //.statusCodeFunction(m -> HttpStatus.OK)
-            )
-            .channel(c -> c.flux("outboundReactive.input"))
-            .get();
+        return IntegrationFlow
+                .from(
+                        WebFlux
+                                .inboundGateway("/all")
+                                .requestMapping(m -> m.methods(HttpMethod.GET))
+                        // .requestPayloadType(ResolvableType.forClassWithGenerics(Flux.class, String.class))
+                        //.statusCodeFunction(m -> HttpStatus.OK)
+                )
+                .channel(c -> c.flux("outboundReactive.input"))
+                .get();
     }
 
-//    @Bean
+    //    @Bean
 //    public MessageChannel fluxResultChannel() {
 //        return new FluxMessageChannel();
 //    }
     @Bean
     public IntegrationFlow outboundReactive() {
         return f -> f
-            .handle(
-                WebFlux
-                    .<MultiValueMap<String, String>>outboundGateway(
-                        m -> UriComponentsBuilder
-                            .fromUriString("http://localhost:8080/posts")
-                            //.queryParams(m.getPayload())
-                            .build()
-                            .toUri()
-                    )
-                    .httpMethod(HttpMethod.GET)
-                    .expectedResponseType(String.class)
-            );
+                .handle(
+                        WebFlux
+                                .<MultiValueMap<String, String>>outboundGateway(
+                                        m -> UriComponentsBuilder
+                                                .fromUriString("http://localhost:8080/posts")
+                                                //.queryParams(m.getPayload())
+                                                .build()
+                                                .toUri()
+                                )
+                                .httpMethod(HttpMethod.GET)
+                                .expectedResponseType(String.class)
+                );
     }
 
 }
