16036e730c0214e8d4812c62dd01dcceb2f8b098
hantsy <hantsy@gmail.com>
2023-01-01 16:14:24 +0800

    chore: clean boot-neo4j codes

--- boot-neo4j/src/test/java/com/example/demo/PostRepositoryTest.java
+++ boot-neo4j/src/test/java/com/example/demo/PostRepositoryTest.java
@@ -1,20 +1,28 @@
 package com.example.demo;
 
-
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.neo4j.DataNeo4jTest;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+import org.testcontainers.containers.Neo4jContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 import reactor.core.publisher.Flux;
 import reactor.test.StepVerifier;
 
 import java.io.IOException;
+import java.time.Duration;
 import java.util.Comparator;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
@@ -24,9 +32,23 @@ import java.util.stream.Stream;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @DataNeo4jTest
+// @Transactional(propagation = Propagation.NOT_SUPPORTED) // reactive is not supported
+@Testcontainers
 @Slf4j
+@TestInstance(TestInstance.Lifecycle.PER_METHOD)
 @ActiveProfiles("test")
 public class PostRepositoryTest {
+
+    @Container
+    static Neo4jContainer<?> neo4jContainer = new Neo4jContainer<>("neo4j:5")
+            .withStartupTimeout(Duration.ofMinutes(5));
+
+    @DynamicPropertySource
+    static void neo4jProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.neo4j.uri", neo4jContainer::getBoltUrl);
+        registry.add("spring.neo4j.authentication.username", () -> "neo4j");
+        registry.add("spring.neo4j.authentication.password", neo4jContainer::getAdminPassword);
+    }
 
     @Autowired
     private PostRepository posts;
