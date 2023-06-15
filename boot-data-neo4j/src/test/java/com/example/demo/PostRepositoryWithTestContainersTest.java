d4c45d4496f5f0aafb3bc9b7ce56bdea2781e841
Raja Kolli <rajadilipkolli@gmail.com>
2023-06-15 09:57:00 +0000

    using testcontaines in dev mode

--- boot-data-neo4j/src/test/java/com/example/demo/PostRepositoryWithTestContainersTest.java
+++ boot-data-neo4j/src/test/java/com/example/demo/PostRepositoryWithTestContainersTest.java
@@ -7,16 +7,11 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.data.neo4j.DataNeo4jTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.data.domain.Example;
 import org.springframework.data.domain.Sort;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
-import org.testcontainers.containers.Neo4jContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
 import reactor.core.publisher.Flux;
 import reactor.test.StepVerifier;
 
@@ -29,25 +24,10 @@ import java.util.stream.Stream;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
-//@DataNeo4jTest
-@Testcontainers
 @Slf4j
-// Testcontainers does not work well with per_class testinstance.
-// see: https://stackoverflow.com/questions/61357116/exception-mapped-port-can-only-be-obtained-after-the-container-is-started-when/61358336#61358336
-@TestInstance(TestInstance.Lifecycle.PER_METHOD)
 @ActiveProfiles("test")
+@Import(TestDemoApplication.class)
 public class PostRepositoryWithTestContainersTest {
-
-    @Container
-    static Neo4jContainer<?> neo4jContainer = new Neo4jContainer<>("neo4j:5")
-            .withStartupTimeout(Duration.ofMinutes(5));
-
-    @DynamicPropertySource
-    static void neo4jProperties(DynamicPropertyRegistry registry) {
-        registry.add("spring.neo4j.uri", neo4jContainer::getBoltUrl);
-        registry.add("spring.neo4j.authentication.username", () -> "neo4j");
-        registry.add("spring.neo4j.authentication.password", neo4jContainer::getAdminPassword);
-    }
 
     @Autowired
     private PostRepository posts;
---
+++ boot-data-neo4j/src/test/java/com/example/demo/TestDemoApplication.java
@@ -0,0 +1,25 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.Neo4jContainer;
+
+import java.time.Duration;
+
+@TestConfiguration(proxyBeanMethods = false)
+public class TestDemoApplication {
+
+    @Bean
+    @ServiceConnection
+    Neo4jContainer<?> neo4jContainer() { 
+        return new Neo4jContainer<>("neo4j:5")
+            .withStartupTimeout(Duration.ofMinutes(5));
+    }
+
+  public static void main(String[] args) {
+    SpringApplication.from(DemoApplication::main).with(TestDemoApplication.class).run(args);
+  }
+
+}
