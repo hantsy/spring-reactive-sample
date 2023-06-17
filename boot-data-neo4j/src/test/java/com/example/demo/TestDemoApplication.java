997edb9df8e2eadc2e96139fb54684bb28bf8a2a
hantsy <hantsy@gmail.com>
2023-06-17 13:46:06 +0800

    chore: clean codes

--- boot-data-neo4j/src/test/java/com/example/demo/TestDemoApplication.java
+++ boot-data-neo4j/src/test/java/com/example/demo/TestDemoApplication.java
@@ -1,8 +1,8 @@
 package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
 import org.testcontainers.containers.Neo4jContainer;
 
@@ -13,13 +13,13 @@ public class TestDemoApplication {
 
     @Bean
     @ServiceConnection
-    Neo4jContainer<?> neo4jContainer() { 
+    Neo4jContainer<?> neo4jContainer() {
         return new Neo4jContainer<>("neo4j:5")
-            .withStartupTimeout(Duration.ofMinutes(5));
+                .withStartupTimeout(Duration.ofMinutes(5));
     }
 
-  public static void main(String[] args) {
-    SpringApplication.from(DemoApplication::main).with(TestDemoApplication.class).run(args);
-  }
+    public static void main(String[] args) {
+        SpringApplication.from(DemoApplication::main).with(TestDemoApplication.class).run(args);
+    }
 
 }
