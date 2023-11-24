7033ff21cc93ce260937f648d9d1bec060ce3ef9
hantsy <hantsy@gmail.com>
2023-11-24 11:30:00 +0800

    chore: clean codes

--- cache/src/main/java/com/example/demo/Application.java
+++ cache/src/main/java/com/example/demo/Application.java
@@ -1,6 +1,5 @@
 package com.example.demo;
 
-
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
