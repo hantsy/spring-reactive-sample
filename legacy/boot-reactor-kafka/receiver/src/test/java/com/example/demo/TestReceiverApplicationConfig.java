20e5ff128362fbc45ceec78c9c89596389f910d4
hantsy <hantsy@gmail.com>
2026-05-14 12:35:31 +0800

    chore: move reactor-kafka to legacy

---
+++ legacy/boot-reactor-kafka/receiver/src/test/java/com/example/demo/TestReceiverApplicationConfig.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.kafka.KafkaContainer;
+import org.testcontainers.utility.DockerImageName;
+
+@TestConfiguration(proxyBeanMethods = false)
+public class TestReceiverApplicationConfig {
+
+    @Bean
+    @ServiceConnection
+    KafkaContainer kafkaContainer() {
+        return  new KafkaContainer(DockerImageName.parse("apache/kafka-native").withTag("4.2.0"));
+    }
+
+}
