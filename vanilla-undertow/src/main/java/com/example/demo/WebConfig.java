4a7ac7d7990435657ef96b41e04a34b17f27a2c1
hantsy <hantsy@gmail.com>
2026-05-28 08:06:33 +0800

    chore: clean up

--- vanilla-undertow/src/main/java/com/example/demo/WebConfig.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.reactive.config.EnableWebFlux;
-
-@Configuration
-@EnableWebFlux
-public class WebConfig {
-}
